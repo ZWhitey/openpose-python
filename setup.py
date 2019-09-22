56329d39f591fc19c2442a1517af7b3db26dddb1
Whitey <c1783468@gmail.com>
2019-09-22 23:57:23 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
-from setuptools import setup, Distribution
+from setuptools import setup
 
-
-class BinaryDistribution(Distribution):
-    def has_ext_modules(foo):
-        return True
 
 setup(name='python-openpose',
-      version='1.0.0',
+      version='1.5.1',
       description='OpenPose for Pypi',
       author='Whitey',
-      url='https://www.python.org/',
+      url='https://github.com/ZWhitey/openpose-python',
       license='MIT',
       keywords='openpose',
       packages=['openpose'],
       install_requires=[],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'Programming Language :: Python :: 3.6'
+
+        ],
     include_package_data=True,
     package_data={'openpose':['*.pyd']},
-      python_requires='>=3'
+      python_requires='~=3.6'
      )
